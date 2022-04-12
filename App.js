8b4b7ebe47f44eda46ec118378a3a692257f54ec
dbladva <ladvadharmesh03@gmail.com>
2022-04-12 18:57:11 +0530

    fsdfs
    sfsdf

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Hello from './src/container/Hello'
 import MedicineData from './src/container/MedicineData'
+import Destructuring from './src/container/Desturcturing'
 
 export default function App() {
   return (
     <View>
       <MedicineData />
+    <Destructuring />
     </View>
   )
 }
