e4c48aa859b48e3e928489e79b22be299498df83
dbladva <ladvadharmesh03@gmail.com>
2022-04-14 18:13:07 +0530

    StateProps
    State and props

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import Hello from './src/container/Hello'
 import MedicineData from './src/container/MedicineData'
 import Destructuring from './src/container/Desturcturing'
+import Medi_State_Fun from './src/container/Medi_State_Fun'
+import Medi_State_class from './src/container/Medi_State_class'
+import Props_Medi_Demo from './src/container/Props_Medi_Demo'
 
 export default function App() {
   return (
     <View>
-      <MedicineData />
-    <Destructuring />
-    </View>
+      {/* <Medi_State_Fun /> */}
+      {/* <Medi_State_class /> */}
+      <Props_Medi_Demo />
+      </View>
   )
 }
