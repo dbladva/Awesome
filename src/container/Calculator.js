b6314a75938b4d6d78d85818a497c8691d4e6f47
dbladva <ladvadharmesh03@gmail.com>
2022-04-19 18:14:06 +0530

    Edit Cal.
    calculator

--- src/container/Calculator.js
+++ src/container/Calculator.js
@@ -3,25 +3,23 @@ import React from 'react'
 
 export default function Calculator() {
 
-  const arr = ["C", "()", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="]
+  const arr = [" C ", " () ", " % ", " / ", " 7 ", " 8 ", " 9 ", " * ", " 4 ", " 5 ", " 6 ", " - ", " 1 ", " 2 ", " 3 ", " + ", "+/-", " 0 ", " . ", " = "]
 
   arr.map((a) => {
     console.log(a);
   })
   return (
     <View style={styles.container}>
-      <View style={styles.InputView}>
+      <View style={styles.InputView}> 
       </View >
       <View style={styles.btnMainView}>
         {
           arr.map((Key, index) => {
-          
               return (
                 <View style={styles.Button}>
                   <Text style={styles.btnText}>{Key}</Text>
                   </View>
               )
-          
           })
         }
       </View>
@@ -31,40 +29,40 @@ export default function Calculator() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
     flex: 1,
     flexDirection: 'column',
     backgroundColor: 'black',
+    padding: 10,
   },
   InputView: {
     flex: 1,
-    borderBottomColor: 'white',
-    borderBottomWidth: 0.5,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     justifyContent: 'center'
   },
   btnMainView: {
-    width: '100%',
-    height: '50%',  
+   flex: 1, 
+    marginTop: 20,
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
   btnText: {
-    color: "white",
+    marginTop: 5,
+    color: "white",   
+    padding: 15,
     fontSize: 30,
-    padding: 5,
-    // borderWidth: 1,
-    // borderColor: 'red',
-    // borderRadius: 60 , 
+    borderWidth: 0.01,
+    borderColor: 'red',
+    borderRadius: 200 , 
+    textAlign: 'center',
+    backgroundColor: "rgba(31,31,31,255)", 
   },
   Button: {
-    alignItems: 'center',
-    justifyContent: 'center',
     minWidth: '25%',
     minHeight: '20%',
-    flex: 2,
-   
-  }
-
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
