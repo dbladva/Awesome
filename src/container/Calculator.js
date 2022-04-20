dc0c9236c596ff797071aefe76bf983c4a0c73dc
Ladva Dharmesh <ladvadharmesh03@gmail.com>
2022-04-20 11:44:57 +0530

    caluclator
    ffs

--- src/container/Calculator.js
+++ src/container/Calculator.js
@@ -1,73 +1,117 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 export default function Calculator() {
   const arr = [
-    ' C ',
-    ' () ',
-    ' % ',
-    ' / ',
-    ' 7 ',
-    ' 8 ',
-    ' 9 ',
-    ' * ',
-    ' 4 ',
-    ' 5 ',
-    ' 6 ',
-    ' - ',
-    ' 1 ',
-    ' 2 ',
-    ' 3 ',
-    ' + ',
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
     '+/-',
-    ' 0 ',
-    ' . ',
-    ' = ',
+    '0',
+    '.',
+    '=',
   ];
 
-  arr.map(a => {
-    console.log(a);
-  });
+  const [equation, setEquation] = useState('');
+
+  const handelEuation = (k) => {
+      setEquation(equation + k)
+  }
+
+  const handelClear = () => {
+    setEquation(' ');
+  }
+
+  const handelDelet = () => {
+    setEquation(equation.substring(0,equation.length - 1))
+  }
+  const handelEqual = () => {
+    if(equation !== '') {
+      let result = eval(equation)
+      setEquation(result)
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <View style={styles.InputView}></View>
+      <View style={styles.InputView}>
+        <Text style={styles.InputText}>{equation}</Text>
+      </View>
       <View style={styles.btnMainView}>
-        {arr.map((Key, index) => {
+        {arr.map((key, index) => {
           return (
-            <View style={styles.Button}>
-              {/* <View style={index == 0 ? {color: 'red'} : {color : 'white'} }> */}
+            key === '+' || key === '-' || key === '*' || key === '/' || key === '%' ?
+              <View style={styles.Button}>
+                <Text
+                  style={
+                    index == 0 || index == 1 || index == 2 || index == 3 || index == 7 || index == 11 || index == 15 || index == 19
+                      ? styles.red
+                      : styles.white
+                  } onPress={() => handelEuation(key)} >
+                  {key}
+                </Text>
+              </View>
+              : key === 'C' ?
+                <View style={styles.Button}>
+                  <Text
+                    style={
+                      index == 0 || index == 1 || index == 2 || index == 3 || index == 7 || index == 11 || index == 15 || index == 19
+                        ? styles.red
+                        : styles.white
+                    } onPress={() => handelClear()} >
+                    {key}
+                  </Text>
+                </View>
 
-              {/* <Text style={styles.btnText} >{Key}</Text> */}
+                : key === 'DEL' ? 
+                <View style={styles.Button}>
+                <Text
+                  style={
+                    index == 0 || index == 1 || index == 2 || index == 3 || index == 7 || index == 11 || index == 15 || index == 19
+                      ? styles.red
+                      : styles.white
+                  } onPress={() => handelDelet()} >
+                  {key}
+                </Text>
+              </View>
+              : key === '=' ?
+              <View style={styles.Button}>
               <Text
                 style={
-                  index == 0 || index == 1 || index == 2 || index==3 || index == 7 || index == 11 || index == 15 || index == 19
-                    ? {
-                        marginTop: 5,
-                        color: 'green',
-                        padding: 18,
-                        fontSize: 30,
-                        borderWidth: 0.01,
-                        borderColor: 'red',
-                        borderRadius: 200,
-                        textAlign: 'center',
-                        backgroundColor: 'rgba(31,31,31,255)',
-                      }
-                    : {
-                        marginTop: 5,
-                        color: 'white',
-                        padding: 18,
-                        fontSize: 30,
-                        borderWidth: 0.01,
-                        borderColor: 'red',
-                        borderRadius: 200,
-                        textAlign: 'center',  
-                        backgroundColor: 'rgba(31,31,31,255)',
-                      }
-                }>
-                {Key}
+                  index == 0 || index == 1 || index == 2 || index == 3 || index == 7 || index == 11 || index == 15 || index == 19
+                    ? styles.red
+                    : styles.white
+                } onPress={() => handelEqual()} >
+                {key}
               </Text>
-            </View>
-          );
+            </View> 
+
+            :
+            <View style={styles.Button}>
+            <Text
+              style={
+                index == 0 || index == 1 || index == 2 || index == 3 || index == 7 || index == 11 || index == 15 || index == 19
+                  ? styles.red
+                  : styles.white
+              } onPress={() => handelEuation(key)} >
+              {key}
+            </Text>
+          </View> 
+          )
         })}
       </View>
     </View>
@@ -90,10 +134,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   btnMainView: {
-    flex: 1,
+    flex: 1.2,
     marginTop: 20,
     flexDirection: 'row',
     flexWrap: 'wrap',
+   
+  },
+  InputText: {
+    fontSize: 35,
+    color: 'white',
+    position: 'absolute',
+    bottom: 0,
+    right: 12,
   },
   btnText: {
     marginTop: 5,
@@ -112,4 +164,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  red: {
+    marginTop: 5,
+    color: 'green',
+    fontSize: 30,
+    borderWidth: 0.01,
+    borderColor: 'red',
+    borderRadius: 100,
+    textAlign: 'center',
+  },  
+  white: {
+    marginTop: 5,
+    color: 'white',
+    fontSize: 30,
+    borderWidth: 0.01,
+    borderColor: 'red',
+    borderRadius: 200,
+    textAlign: 'center',
+  }
 });
